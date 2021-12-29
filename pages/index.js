7500dff5cd76dfcb94679f3ebd823e488ae2fbe7
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-12-29 08:37:32 +0100

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-3xl font-bold">
+          Welcome to{' '}
+          <a className="text-blue-600" href="https://nextjs.org">
+            NACOS KWASU WebProject | <br /> Bootstraped with Next.js +
+            TailwindCSS
+          </a>
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Get started by editing{' '}
+          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+            pages/index.js
+          </code>
+        </p>
+      </main>
+    </div>
+  )
+}
